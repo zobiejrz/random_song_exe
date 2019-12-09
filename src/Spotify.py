456a7e98d4937d5b9af902206aac8784a892b7a0
Ben Zobrist <ben@zobie.com>
2019-12-08 21:58:36 -0700

    Added Spotify Project and updated Status

---
+++ src/Spotify.py
@@ -0,0 +1,80 @@
+import json
+import requests
+import os
+from random import randint
+import random
+import base64
+from Song import Song
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+
+class Spotify:
+
+    spotify_client = 0
+    spotify_secret = 0
+
+    def __init__(self, client=0, secret=0):
+        self.spotify_client = client
+        self.spotify_secret = secret
+
+    def GetClientCredentials(self):
+        """
+        Gets the Spotify Client credentials needed to perform api calls
+        """
+
+        # Make authorization code
+        str = "{}:{}".format(self.spotify_client, self.spotify_secret)
+        utf_str = str.encode("UTF-8")
+        auth_code = base64.b64encode(utf_str)
+
+        # Make the POST request
+        header = {"Authorization": "Basic {}".format(auth_code)}
+        data = {'grant_type' : "client_credentials"}
+        url = "https://accounts.spotify.com/api/token"
+        response = requests.post(url, data=data, auth = (self.spotify_client, self.spotify_secret))
+
+        creds = response.json() # credential object
+
+        return creds['access_token']
+
+    def GetRandomSong(self):
+        """
+        Gets a random song by searching spotify using wotd and returning a random offset
+        """
+
+        # Get the wotd
+        dir = os.path.join(THIS_FOLDER, "wotd.json")
+        wotd = {} # The word that is used in the search
+        with open(dir) as d:
+            wotd = json.load(d)
+
+        # Get the json from spotify
+        query_url = "https://api.spotify.com/v1/search?q={}&type=track&offset={}&limit=1".format(wotd["wotd"], random.randint(0, 10000))
+        header = {"Authorization": "Bearer {}".format(self.GetClientCredentials())}
+        response = requests.get(query_url, headers=header)
+        item = response.json()["tracks"]["items"][0]
+
+        # Parse variables
+        artist = item["album"]["artists"][0]["name"]
+        name = item["name"]
+        link = item["external_urls"]["spotify"]
+
+        # Make a song object+return
+        newSong = Song(name, artist, link)
+        return newSong
+
+
+### FOR TESTING ###
+
+# Gets spotify authentication keys
+# auth_file = os.path.join(THIS_FOLDER, "../auth.json")
+# auth = {}
+# with open(auth_file) as a:
+#     auth = json.load(a)
+# spotify_client = auth['spotify_client']
+# spotify_secret = auth['spotify_secret']
+#
+# # Make Spotify Object + GetCredentials
+# spotify = Spotify(spotify_client, spotify_secret)
+# print(spotify.GetClientCredentials())
--- src/__main__.py
+++ src/__main__.py
@@ -6,60 +6,30 @@
 #                                                #
 ##################################################
 
-from datetime import datetime # IMPORTANT
-from datetime import time # IMPORTANT
-from datetime import timedelta # IMPORTANT
-import time as t
+from datetime import * # IMPORTANT
 import os # IMPORTANT
 import json # IMPORTANT
 import threading # IMPORTANT
 import requests # IMPORTANT
 from Song import *
-from Album import *
-import random
-import base64 # IMPORTANT
+# import random
+# import base64 # IMPORTANT
 import tweepy
+from Spotify import Spotify
+botShouldRun = False
 
+def PrintOptions():
+    print("\nOPTIONS")
+    print("%-10s" % "status" + "Prints the current status of the bot.")
+    print("%-10s" % "wotd" + "Change the word used to search for songs.")
+    print("%-10s" % "freq" + "Change the frequency of tweets.")
+    print("%-10s" % "toggle" + "Turns the bot on/off")
+    print("%-10s" % "tweet" + "Sends a tweet as the bot. Doesn't affect scheduling.")
+    print("%-10s" % "options" + "Display options.")
+    print("%-10s" % "quit" + "Turns the bot off.\n")
 
-
-def GetClientCredentials(spotify_client, spotify_secret):
-    """Gets the Spotify Client credentials needed to perform api calls"""
-    s = "{}:{}".format(spotify_client, spotify_secret)
-    b = s.encode("UTF-8")
-    e = base64.b64encode(b)
-
-    header = {"Authorization": "Basic {}".format(e)}
-    data = {'grant_type' : "client_credentials"}
-
-    url = "https://accounts.spotify.com/api/token"
-    a = requests.post(url, data=data, auth = (spotify_client, spotify_secret))
-
-    creds = a.json()
-    return creds['access_token']
-
-def GetRandomSong(sc, ss):
-    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    dir = os.path.join(THIS_FOLDER, "wotd.json")
-    wotd = {}
-    with open(dir) as d:
-        wotd = json.load(d)
-
-    url = "https://api.spotify.com/v1/search?q={}&type=track&offset={}&limit=1".format(wotd["wotd"], random.randint(0, 10000))
-    header = {"Authorization": "Bearer {}".format(GetClientCredentials(sc, ss))}
-    a = requests.get(url, headers=header)
-
-    # dir = os.path.join(THIS_FOLDER, "output.json")
-    # with open(dir, "w") as d:
-    #     json.dump(a.json(), d)
-
-    item = a.json()["tracks"]["items"][0]
-
-    artist = item["album"]["artists"][0]["name"]
-    name = item["name"]
-    link = item["external_urls"]["spotify"]
-
-    newSong = Song(name, artist, link)
-    return newSong
+def GetUTCTime():
+    return "UTC: {}".format(datetime.utcnow())
 
 def SendTweet(input):
     try:
@@ -72,6 +42,8 @@ def Random_Song_Exe(sc, ss):
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     dir = os.path.join(THIS_FOLDER, "data.json")
     while True:
+        if not botShouldRun:
+            break;
         data = {}
         with open(dir) as d:
             data = json.load(d)
@@ -85,7 +57,7 @@ def Random_Song_Exe(sc, ss):
         currentTD = current_time - time_of_last_tweet
         if currentTD > TIME_BETWEEN_UPDATES:
             # Get Song Data
-            song = GetRandomSong(sc, ss)
+            song = spotify.GetRandomSong()
             # Tweet
 
             SendTweet(str(song))
@@ -99,8 +71,31 @@ def Random_Song_Exe(sc, ss):
 # Pull in the data from the last time the bot was active
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-could_connect_to_spotify = True # In the future, random_song_exe will actually check if it can connect
-could_connect_to_twitter = True # Now, the connection is assumed
+# Get keys from auth.json
+auth_file = os.path.join(THIS_FOLDER, "auth.json")
+auth = {}
+with open(auth_file) as a:
+    auth = json.load(a)
+
+# Get all the keys+credentials
+spotify_client = auth['spotify_client']
+spotify_secret = auth['spotify_secret']
+twitter_client = auth['twitter_client']
+twitter_secret = auth['twitter_secret']
+twitter_access_token = auth['twitter_access_token']
+twitter_access_token_secret = auth['twitter_access_token_secret']
+
+# Set up Tweepy
+auth = tweepy.OAuthHandler(twitter_client, twitter_secret)
+auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+api = tweepy.API(auth)
+
+# Set up spotify
+spotify = Spotify(spotify_client, spotify_secret)
+
+# Set up bot threads
+bot = threading.Thread()
+main_thread = threading.current_thread()
 
 print("##################################################\n" +
       "#                                                #\n" +
@@ -109,90 +104,69 @@ print("##################################################\n" +
       "#             © Ben Zobrist 2019                 #\n" +
       "#                                                #\n" +
       "##################################################")
+print( "\n{}\n".format(GetUTCTime()) )
+print( "Type 'options' to see available commands. Type 'quit' to close the program\n" )
+# Begin CLI
+running = True
+while running:
+    user_input = input(" >>> ")
 
-# Get keys from auth.json
-auth_file = os.path.join(THIS_FOLDER, "auth.json")
-auth = {}
-with open(auth_file) as a:
-    auth = json.load(a)
+    if user_input == "status":
+        i = 0
+        for t in threading.enumerate():
+            i+=1
 
-# Connect to Spotify
+        print("\nSTATUS")
+        print("%-30s" % "Is Active" + "{}".format("[ ACTIVE ]" if bot.isAlive() else "[INACTIVE]"))
+        print("%-30s" % "Number of Threads Active" + "{}".format(i))
+        print("%-30s" % "Time of next tweet" + "{}".format('datetime'))
+        print("%-30s" % "Frequency of tweets" + "{}".format('datetime'))
+        print()
+        print("%-30s" % "WOTD" + "{}".format('word'))
+        print("%-30s" % "Number of Tweets" + "{}".format(api.me().statuses_count))
+        print()
 
-spotify_client = auth['spotify_client']
-spotify_secret = auth['spotify_secret']
-
-# Connect to Twitter
-
-twitter_client = auth['twitter_client']
-twitter_secret = auth['twitter_secret']
-twitter_access_token = auth['twitter_access_token']
-twitter_access_token_secret = auth['twitter_access_token_secret']
-
-auth = tweepy.OAuthHandler(twitter_client, twitter_secret)
-auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-api = tweepy.API(auth)
-
-# Ask whether to bootup if both connnections were successful
-
-obtained_valid_input = False
-should_start = ""
-while obtained_valid_input == False:
-    should_start = input("\nWould you like to start? [y/n]: ")
-    if should_start == "y" or should_start == "n":
-        obtained_valid_input = True
-    else:
-        print("I didn't quite get that, please type 'y' for yes, or 'n' for no.")
-
-if should_start == "y":
-    print("\n[ ACTIVE ]")
-    print("Type 'quit' to turn off the bot. Push the enter key for other commands.\n")
-    # Thread 1: Manage Tweets as expected -- Check delta times, etc...
-    bot = threading.Thread(target=Random_Song_Exe, args=(spotify_client, spotify_secret,), daemon=True)
-    bot.start()
-
-    # Main Thread: User Input+Commands
-    main_thread = threading.current_thread()
-    user_input = ""
-    while user_input != "quit":
-        user_input = input(" >>> ")
-
-        if user_input == "status":
-            i = 0
-            for t in threading.enumerate():
-                if t == main_thread:
-                        continue
-                i+=1
-            print("\nSTATUS")
-            print("The bot is{} active.{}".format(" not" if i == 0 else "", " {} threads are active.".format(i) if i > 1 else ""))
-            print("The last automated tweet was sent at {}.".format("datetime"))
-            print("The next automated tweet will be sent at {}.\n".format("datetime"))
-
-        elif user_input == "tweet":
-            valid_length = False
-            status_content = ""
-
-            while not valid_length:
-
-                print("\nType out your tweet. Leave the field blank to cancel.")
-                status_content = input()
-
-                if len(status_content) > 280:
-                    print("Tweets can only be up to 280 characters. Yours is {} characters.".format(len(status_content)))
-                else:
-                    valid_length = True
-
-            if len(status_content) != 0:
-                send_tweet = threading.Thread(target=SendTweet, args=(status_content,))
-                send_tweet.start()
-            else:
-                print("Cancelled.\n")
-
-        elif user_input != "quit":
-            print("\nOPTIONS")
-            print("%-10s" % "status " + "Prints the current status of the bot.")
-            print("%-10s" % "tweet " + "Sends a tweet as the bot. Doesn't affect scheduling.")
-            print("%-10s" % "quit " + "Turns the bot off.\n")
+    elif user_input == "wotd":
+        pass
+    elif user_input == "freq":
+        pass
+    elif user_input == "toggle":
+        if bot.isAlive():
+            botShouldRun = False
+            print ( "[INACTIVE]" )
+            print ( "Bot Terminated\n" )
+            bot.join()
         else:
-            print("[INACTIVE]")
+            bot = threading.Thread(target=Random_Song_Exe, args=(spotify_client, spotify_secret,), daemon=True)
+            botShouldRun = True
+            print ( "[ ACTIVE ]" )
+            print ( "Bot Activated\n" )
+            bot.start()
 
-print("\n\tGoodbye!\n")
+    elif user_input == "tweet":
+        valid_length = False
+        status_content = ""
+
+        while not valid_length:
+
+            print("\nType out your tweet. Leave the field blank to cancel.")
+            status_content = input()
+
+            if len(status_content) > 280:
+                print("Tweets can only be up to 280 characters. Yours is {} characters.".format(len(status_content)))
+            else:
+                valid_length = True
+
+        if len(status_content) != 0:
+            send_tweet = threading.Thread(target=SendTweet, args=(status_content,))
+            send_tweet.start()
+        else:
+            print("Cancelled.\n")
+    elif user_input == "options":
+        PrintOptions()
+    elif user_input == "quit":
+        running = False
+    else:
+        print( "\n'{}' isn't understood input. Type 'options' to see valid commands.\n".format(user_input) )
+
+print( "\n\tGoodbye!\n" )
